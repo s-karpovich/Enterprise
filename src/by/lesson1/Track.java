a3c096bb1c4aa2bcef8a4d6d538e2571aa7387a9
s-karpovich <mdcatalog2@gmail.com>
2019-04-01 04:00:53 +0300

    ee

---
+++ src/by/lesson1/Track.java
@@ -0,0 +1,27 @@
+package by.lesson1;
+
+class Track {
+    private String name;
+    private int length;
+
+    Track(String name, int minLength, int maxLength) {
+        this.name = name;
+        this.length = minLength + (int) (Math.random() * (maxLength - minLength + 1));
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+}
